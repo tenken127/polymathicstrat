ea7a15dff5944b9d00ba4724e4c4b7449a93d1a6
tenken127 <dagofarias@gmail.com>
2026-04-21 12:14:22 -0400

    build: include trades.html in vite build inputs

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         strategies: resolve(__dirname, 'strategies.html'),
+        trades: resolve(__dirname, 'trades.html'),
       }
     }
   },
